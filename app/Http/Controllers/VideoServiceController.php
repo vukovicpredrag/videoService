a63e4f00565f9437f93217ba83b59265a26928e7
Predrag Vukovic <vukovicpredrag90@gmail.com>
2022-07-27 18:24:44 +0200

    Polishing video service details

--- app/Http/Controllers/VideoServiceController.php
+++ app/Http/Controllers/VideoServiceController.php
@@ -26,45 +26,48 @@ class VideoServiceController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function getInfo(Request $request): JsonResponse
+    public function getInfo(Request $request)//: JsonResponse
     {
         $videoUrl = $request->video_url;
 
-        //Verify that the URL parameter has been entered
-        if(!$videoUrl){
+        if (!$videoUrl) {
 
-            return response()->json("Video URL parameter missing. Please enter a video URL!");
+            //Response if request is missing a required parameter | 422 (Unprocessable Entity)
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'Video URL parameter missing. Please enter a video URL!',
+                'data' => []
+            ], 422);
 
         }
 
         //Detect service provider
         $provider = $this->detectProvider($videoUrl);
 
+        if (!$provider) {
+
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'Video URL is not valid!!',
+                'data' => []
+            ], 400);
+
+        }
+
         //Get video ID form vide URL
         $videoId = $this->getVideoId($videoUrl, $provider);
 
-        //Prepare a request URL based on the provider's
-        switch($provider) {
-            case('youtube'):
-                $serviceUrl = $this->youTubeService;
-                break;
+        //Get a service, request URL based on the provider's
+        $serviceUrl = $this->getServiceUrl($provider);
 
-            case('vimeo'):
-                $serviceUrl = $this->vimeoService;
-                break;
-
-            default:
-                $serviceUrl = '';
-        }
-
-        $requestUrl = str_replace ("<VIDEO_ID>", $videoId, $serviceUrl);
+        $requestUrl = str_replace("<VIDEO_ID>", $videoId, $serviceUrl);
 
         $getVideoInfo = $this->getVideoInfo($requestUrl);
 
         return new JsonResponse([
             'success' => true,
             'message' => 'Video information.',
-            'data'    => $getVideoInfo
+            'data' => $getVideoInfo
         ], 200);
     }
 
@@ -82,15 +85,7 @@ class VideoServiceController extends Controller
         $vimeoRx = '/(https?:\/\/)?(www\.)?(player\.)?vimeo\.com\/([a-z]*\/)*([Ã¢Â€ÂŒÃ¢Â€Â‹0-9]{6,11})[?]?.*/';
         $matchVimeo = preg_match($vimeoRx, $videoUrl);
 
-        $type = 'none';
-
-        if ($matchYouTube) {
-            $type = 'youtube';
-        }
-
-        if($matchVimeo) {
-            $type = 'vimeo';
-        }
+        $type = $matchYouTube ? 'youtube' : ($matchVimeo ? 'vimeo' : '');
 
         return $type;
     }
@@ -105,23 +100,23 @@ class VideoServiceController extends Controller
      */
     public function getVideoId($videoUrl, $provider)
     {
-        if($provider == 'youtube') {
+        if ($provider == 'youtube') {
 
             $parseVideoURL = parse_url($videoUrl);
 
             parse_str($parseVideoURL['query'], $results);
 
-            $videoId =  $results['v'];
+            $videoId = $results['v'];
 
         }
 
-        if($provider == 'vimeo') {
+        if ($provider == 'vimeo') {
 
-            $videoId = (int) substr(parse_url($videoUrl, PHP_URL_PATH), 1);
+            $videoId = (int)substr(parse_url($videoUrl, PHP_URL_PATH), 1);
 
         }
 
-        //return empty sting if video not found
+        //Return empty sting if video not found
         $videoId = $videoId ?: '';
 
         return $videoId;
@@ -135,25 +130,53 @@ class VideoServiceController extends Controller
      */
     public function getVideoInfo($requestUrl)
     {
+        $resposnse = [];
+
         $requestUrl = str_replace('http', 'https', $requestUrl);
 
         //Set URL request
-        $ch = curl_init( $requestUrl);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        $curlResponse = curl_exec( $ch );
-        curl_close( $ch );
+        $ch = curl_init($requestUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $curlResponse = curl_exec($ch);
+        curl_close($ch);
 
         $curlResponse = json_decode($curlResponse);
 
         //Prepare API response
-        $resposnse = [
-            'author' => $curlResponse->author_name,
-            'title'  => $curlResponse->title,
-            'thumbnail_url' => $curlResponse->thumbnail_url,
-        ];
+        if( $curlResponse ) {
+            $resposnse = [
+                'author' => $curlResponse->author_name,
+                'title' => $curlResponse->title,
+                'thumbnail_url' => $curlResponse->thumbnail_url,
+            ];
+        }
 
         return $resposnse;
     }
 
+
+    /**
+     * Get service url (Youtube | Vimeo)
+     *
+     * @param string $provider
+     * @return string
+     */
+    public function getServiceUrl($provider)
+    {
+        switch($provider) {
+            case('youtube'):
+                $serviceUrl = $this->youTubeService;
+                break;
+
+            case('vimeo'):
+                $serviceUrl = $this->vimeoService;
+                break;
+
+            default:
+                $serviceUrl = '';
+        }
+
+        return $serviceUrl;
+    }
 
 }
